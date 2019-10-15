689ab2c787e6a226c48086a7af60fdf93ed55ee4
jcruz-y-@e1z3r3p6.42.us.org <jcruz-y-@42.us.org>
2019-10-14 18:12:52 -0700

    changed camera

--- gt_rendering/Hand.gd
+++ gt_rendering/Hand.gd
@@ -1,5 +1,5 @@
 extends Spatial
-extends Label
+#extends Label
 
 var socket = PacketPeerUDP.new()
 var status = socket.listen(4242, "127.0.0.1")
@@ -49,21 +49,21 @@ func _process(delta):
 #extends Label
 
 # Timestamps of frames rendered in the last second
-var times := []
+#var times := []
 
 # Frames per second
-var fps := 0
+#var fps := 0
 
 
-func _process(_delta: float) -> void:
-	var now := OS.get_ticks_msec()
+#func _process(_delta: float) -> void:
+#	var now := OS.get_ticks_msec()
 
-	# Remove frames older than 1 second in the `times` array
-	while times.size() > 0 and times[0] <= now - 1000:
-		times.pop_front()
+#	# Remove frames older than 1 second in the `times` array
+#	while times.size() > 0 and times[0] <= now - 1000:
+#		times.pop_front()
 
-	times.append(now)
-	fps = times.size()
+#	times.append(now)
+#	fps = times.size()
 
-	# Display FPS in the label
-	text = str(fps) + " FPS"
+#	# Display FPS in the label
+#	text = str(fps) + " FPS"
